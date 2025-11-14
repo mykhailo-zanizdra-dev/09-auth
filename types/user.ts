6dde9c57b0640fc2cfcc8dd0fe48572d8536ce5b
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2025-11-14 14:49:44 +0200

    feat: Added external api

---
+++ types/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+  username: string;
+  email: string;
+  avatar: string;
+}
+
+export interface UserCredentials {
+  email: string;
+  password: string;
+}
+
+export interface UpdateUserData {
+  username: string;
+}
