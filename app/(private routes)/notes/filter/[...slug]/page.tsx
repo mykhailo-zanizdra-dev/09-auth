c9119705775807ddb6f01ba4709046cf1d677861
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2025-11-14 15:49:25 +0200

    fix: Renamed directories

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -2,7 +2,7 @@ interface FilteredNotesProps {
   params: Promise<{ slug: string[] }>;
 }
 
-import NotesClient from '@/app/(private-routes)/notes/filter/[...slug]/Notes.client';
+import NotesClient from '@/app/(private routes)/notes/filter/[...slug]/Notes.client';
 import QUERY_KEYS from '@/const/queryKeys';
 import { fetchServerNotes } from '@/lib/api/serverApi';
 import {
